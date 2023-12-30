c3bee1555a56edc6ceb9d0d409d33588aceda5e0
Mahaut Windal <mwindal@hotmail.com>
2023-12-30 17:12:13 +0100

    routes opérationnelles

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Book=require('./models/Book')
 const app = express();
+const bookRoutes = require('./routes/book');
 
 mongoose.connect('mongodb+srv://windalmahaut:qoFUyirNbEXYefZ8@cluster0.cczpfwg.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -9,47 +9,16 @@ mongoose.connect('mongodb+srv://windalmahaut:qoFUyirNbEXYefZ8@cluster0.cczpfwg.m
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use(express.json());
 
-app.post('/api/book', (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
 
-app.get('/api/book/:id', (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
 
-app.put('/api/book/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/book/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-app.use('/api/books', (req, res, next) => {
-  Book.find()
-  .then(books => res.status(200).json(books))
-  .catch(error => res.status(400).json({ error }));
-  });
+  app.use('/api/books', bookRoutes)
   module.exports = app;
